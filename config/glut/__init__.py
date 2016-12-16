2c4a3a7d0ce762214223cdd3115498310a9afbf1
Joseph Coffland <joseph@cauldrondevelopment.com>
2016-12-15 17:28:32 -0800

    Updates to glut config

--- config/glut/__init__.py
+++ config/glut/__init__.py
@@ -13,14 +13,11 @@ def configure(conf):
 
     conf.CBRequireHeader('GL/glut.h')
 
-    if env['PLATFORM'] == 'win32' or int(env.get('cross_mingw', 0)):
-        if not conf.CBCheckLib('freeglut_static'):
-            conf.CBRequireLib('glut32')
+    if not (conf.CBCheckLib('glut') or conf.CBCheckLib('glut') or
+            conf.CBCheckLib('freeglut_static') or conf.CBCheckLib('freeglut')):
+        raise 'Missing glut'
 
-        env.CBDefine('FREEGLUT_STATIC')
-        return
-
-    conf.CBRequireLib('glut')
+    env.CBDefine('FREEGLUT_STATIC')
 
 
 def generate(env):
